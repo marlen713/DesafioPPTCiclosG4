86deebcbad79b885061e5fba26570bc6210d031b
marlen713 <ma000161@gmail.com>
2022-03-31 17:11:19 -0300

    initial commit

---
+++ desafioppt.rb
@@ -0,0 +1,39 @@
+# Crear un programa que nos permita jugar "Piedra, papel o tijera".
+# ● Primero debes realizar y validar un diagrama de flujo que cumpla con los
+# requerimientos.
+# ● Luego debes programar la solución utilizando Ruby y tu editor de texto favorito.
+
+# Imprimir en pantalla un menú que permita, al jugador número uno, elegir una opción:
+# Turno jugador uno:
+# ○ Piedra
+# ○ Papel
+# ○ Tijeras
+# ○ Salir
+# ● El programa debe solicitar al jugador número uno el ingreso de una opción.
+# ● Si el jugador número uno ingresa una opción que no pertenece al menú, se debe
+# mostrar en pantalla que la opción no es válida y volver a solicitar -al jugador número
+# uno- el ingreso de una opción.
+# ● Si el jugador número uno la opción ingresada es válida, y no se ingresó la opción de
+# salir (4), se debe desplegar un menú que permita, al jugador número dos, elegir una
+# opción:
+# Turno jugador dos:
+# ○ Piedra
+# ○ Papel
+# ○ Tijeras
+# ○ Salir
+
+# ● Si el jugador número dos ingresa una opción que no es válida, se debe mostrar en
+# pantalla que la opción no es válida y volver a solicitar -al jugador número dos- el
+# ingreso de una opción.
+# ● Si cualquiera de los dos jugadores selecciona la opción número 4, el programa debe
+# terminar.
+# ● Si ambas opciones seleccionadas son válidas, y no son la opción de salir, el
+# programa debe elegir un ganador o un empate de acuerdo al siguiente criterio:
+# ○ Piedra gana a tijera.
+# ○ Papel gana a piedra.
+# ○ Tijera gana a papel.
+# ● El programa, luego de evaluar las selecciones de ambos jugadores, debe imprimir el
+# jugador ganador y luego terminar.
+# ● Si ambos jugadores seleccionaron la misma opción, el programa debe imprimir que
+# el resultado es un empate y luego terminar
+
